b9e9f2d74c73af54895a507ba2a080316009e30e
Ankit kanojiya <94682775+ankitkanojiya07@users.noreply.github.com>
2024-08-31 19:25:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import ListItem from "./components/ListItem";
 import Button from "./components/Button";
-import Counter from "./components/Counter";
 import "./style.css"
 
 const App = () =>{
